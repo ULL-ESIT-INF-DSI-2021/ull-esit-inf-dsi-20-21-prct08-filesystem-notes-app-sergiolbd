dd90285317791052f9699862f68ae6d6db092309
sergiolbd <alu0101244440@ull.edu.es>
2021-04-23 14:37:01 +0100

    add coveralls funciona

--- test/index.spec.ts
+++ test/index.spec.ts
@@ -1,31 +1,13 @@
 import 'mocha';
 import {expect} from 'chai';
-import {Reduce, AddReduce} from '../src/TemplateMethod';
+import {add} from '../src/index';
 
 
 describe('Test', () => {
   // Primer test
-  const inputArray = new AddReduce([1, 2, 3]);
-  inputArray.run();
 
-  it('IinputArray.getArray()', () => {
-    expect(inputArray.getArray()).to.deep.equal([1, 2, 3]);
-  });
 
-  it('AddReduce [1, 2, 3]', () => {
-    // console.log(inputArray.getResult());
-    expect(inputArray.getResult()).to.be.eq(6);
-  });
-
-  const inputArray2 = new AddReduce([2, 2, 4]);
-  inputArray2.run();
-
-  it('IinputArray2.getArray()', () => {
-    expect(inputArray2.getArray()).to.deep.equal([2, 2, 4]);
-  });
-
-  it('AddReduce [2, 2, 4]', () => {
-    // console.log(inputArray.getResult());
-    expect(inputArray2.getResult()).to.be.eq(8);
+  it('2+3 = 5', () => {
+    expect(add(2, 3)).to.be.eq(5);
   });
 });
